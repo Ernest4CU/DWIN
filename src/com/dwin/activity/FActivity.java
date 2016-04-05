4202988afaa8ef3a35c410c49a94525556c68167
Ernest4CU <Ernest_xin@outlook.com>
2016-04-05 12:38:16 +0800

    界面布局基本完成

--- src/com/dwin/activity/FActivity.java
+++ src/com/dwin/activity/FActivity.java
@@ -3,7 +3,10 @@ package com.dwin.activity;
 import android.app.Activity;
 import android.app.Instrumentation;
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,5 +18,10 @@ public class FActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.factivity);
+        DisplayMetrics dm=getResources().getDisplayMetrics();
+        int w_screen = dm.widthPixels;
+        int h_screen = dm.heightPixels;
+
+        Log.e("屏幕测试","宽度：" + w_screen + "高度：" + h_screen + "密度：" + dm.densityDpi);
     }
 }
